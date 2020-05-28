2abff0e366e2aae47b3829bbfb54ef947d9a080e
Renato Lima <renatoslima46@gmail.com>
2020-05-28 15:45:59 -0300

    Routes, controller and models completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.create(name: "Velho barreiro")
+Cocktail.create(name: "Corote de Pêssego")
+Cocktail.create(name: "Balalaika")
