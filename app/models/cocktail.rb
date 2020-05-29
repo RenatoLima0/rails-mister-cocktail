6e4968bfb54be5cd3f71647d89fb3e2ff6b518f7
Renato Lima <renatoslima46@gmail.com>
2020-05-29 17:08:47 -0300

    Complete

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,9 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses, dependent: :destroy
+  has_one_attached :photo
 
+  validates :photo, presence: true
   validates :name, presence: true
   validates :name, uniqueness: true
 end
